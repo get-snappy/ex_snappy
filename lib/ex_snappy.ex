7986c6f20a3960dae1e7b8affa72c0a88f0d86d6
Martin Feckie <martin@mfeckie.dev>
2025-04-16 21:51:50 +0800

    Make snap a noop unless enabled

--- lib/ex_snappy.ex
+++ lib/ex_snappy.ex
@@ -1,133 +1,58 @@
 defmodule ExSnappy do
+  @moduledoc """
+  ExSnappy is a library for taking snapshots of HTML using the Go Snappy service.
+  """
+
+  @doc """
+  Takes a snapshot of the given HTML and sends it to the Go Snappy service.
+
+  Uses the name of the test function as the snapshot name.
+  """
   defmacro snap(html) do
-    test_name = generate_test_name(__CALLER__)
+    if Application.get_env(:ex_snappy, :enabled) do
+      test_name = ExSnappy.Utils.generate_test_name(__CALLER__)
 
-    quote do
-      ExSnappy.process_snapshot(unquote(test_name), unquote(html), %{})
-    end
-  end
-
-  defmacro snap(name, html) do
-    test_name = generate_test_name(__CALLER__, name)
-
-    quote do
-      ExSnappy.process_snapshot(unquote(test_name), unquote(html), %{})
-    end
-  end
-
-  defmacro snap(name, html, options) do
-    test_name = generate_test_name(__CALLER__, name)
-
-    quote do
-      ExSnappy.process_snapshot(
-        unquote(test_name),
-        unquote(html),
-        unquote(options)
-      )
-    end
-  end
-
-  def process_snapshot(name, html, options) do
-    # send HTML and options to go-snappy
-    wrapped = maybe_wrap_html(html)
-
-    html =
-      case Map.get(options, :strip_script_tags) do
-        true -> strip_script_tags(wrapped)
-        _ -> wrapped
+      quote do
+        ExSnappy.API.process_snapshot(unquote(test_name), unquote(html), %{})
       end
-
-    post(name, html, options)
-  end
-
-  def post(name, html, options) do
-    # send HTML and options to go-snappy
-    endpoint = Application.get_env(:ex_snappy, :endpoint, "http://localhost:4050")
-
-    url = Path.join([endpoint, "snappy-api", "snapshot", UUID.uuid4()])
-
-    req_options = Application.get_env(:ex_snappy, :req_options, [])
-
-    body = %{
-      "name" => name,
-      "html" => html,
-      "options" => options
-    }
-
-    options =
-      [json: body]
-      |> Keyword.merge(req_options)
-
-    request = Req.post(url, options)
-
-    case request do
-      {:ok, %Req.Response{status: 200, body: "OK"}} ->
-        :ok
-
-      {:ok, %Req.Response{status: status}} ->
-        {:error, "Error: #{status}"}
-
-      {:error, reason} ->
-        {:error, reason}
     end
   end
 
-  def strip_script_tags(html) do
-    # Remove script tags from the HTML
-    cleansed =
-      Floki.parse_document!(html)
-      |> Floki.traverse_and_update(fn
-        {"script", _, _} -> nil
-        el -> el
-      end)
+  @doc """
+  Takes a snapshot of the given HTML with the specified name 
 
-    # Convert the modified HTML back to a string
-    Floki.raw_html(cleansed)
-  end
+  Uses the name of the test function as the snapshot name plus a suffix. Use if you need to run
+  multiple snapshots in the same test function.
+  """
+  defmacro snap(name, html) do
+    if Application.get_env(:ex_snappy, :enabled) do
+      test_name = ExSnappy.Utils.generate_test_name(__CALLER__, name)
 
-  def maybe_wrap_html(html) do
-    # check if html has a head tag
-    Floki.parse_document!(html)
-    |> Floki.find("head")
-    |> case do
-      [] ->
-        # if not, wrap it in a head and body tag
-        wrapper_fn = Application.get_env(:ex_snappy, :wrapper_fn)
-
-        if is_function(wrapper_fn) do
-          # if wrapper_fn is a function, call it with the html
-          wrapper_fn.(html)
-        else
-          # if not, return the html as is
-          html
-        end
-
-      _ ->
-        # if it does, return the html as is
-        html
+      quote do
+        ExSnappy.API.process_snapshot(unquote(test_name), unquote(html), %{})
+      end
     end
   end
 
-  defp generate_test_name(caller) do
-    case caller.function do
-      {test_name, _arity} ->
-        test_name
-        |> to_string()
-        |> String.trim_leading("test ")
+  @doc """
+  Takes a snapshot of the given HTML with the specified name and options.
 
-      _ ->
-        raise("TestSnapshot.snapshot/1 must be called from a test function")
-    end
-  end
+  Uses the name of the test function as the snapshot name plus a suffix. Use if you need to run
+  multiple snapshots in the same test function.
 
-  defp generate_test_name(caller, name) do
-    case caller.function do
-      {test_name, _arity} ->
-        trimmed_name = test_name |> to_string() |> String.trim_leading("test ")
-        "#{trimmed_name} - #{name}"
+  Options are passed to the Go Snappy service to control the rendered screenshots.
+  """
+  defmacro snap(name, html, options) do
+    if Application.get_env(:ex_snappy, :enabled) do
+      test_name = ExSnappy.Utils.generate_test_name(__CALLER__, name)
 
-      _ ->
-        raise("TestSnapshot.snapshot/1 must be called from a test function")
+      quote do
+        ExSnappy.API.process_snapshot(
+          unquote(test_name),
+          unquote(html),
+          unquote(options)
+        )
+      end
     end
   end
 end
