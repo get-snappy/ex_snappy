00fb0a11a01aa29ab8721263bd6a03263cbef47d
Martin Feckie <martin@mfeckie.dev>
2025-04-16 21:11:42 +0800

    Start adding docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule ExSnappy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "ExSnappy",
+      source_url: "",
+      docs: &docs/0
     ]
   end
 
@@ -19,10 +22,15 @@ defmodule ExSnappy.MixProject do
     ]
   end
 
+  defp docs do
+    []
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:elixir_uuid, "~> 1.2.1"},
+      {:ex_doc, "0.37.3", only: :dev, runtime: false},
       {:floki, "0.37.1"},
       {:jason, "~> 1.4"},
       {:req, "~> 0.5"},
