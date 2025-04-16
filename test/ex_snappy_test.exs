bb32ab53a57758b7fef52567b4c7ba41c583b667
Martin Feckie <martin@mfeckie.dev>
2025-04-16 21:52:19 +0800

    Separate tests

--- test/ex_snappy_test.exs
+++ test/ex_snappy_test.exs
@@ -2,55 +2,20 @@ defmodule ExSnappyTest do
   use ExUnit.Case
   doctest ExSnappy
 
-  test "Simple success" do
+  test "Without name" do
+    Req.Test.stub(ExSnappy, fn conn ->
+      Req.Test.text(conn, "OK")
+    end)
+
+    assert ExSnappy.snap("<html></html>") == :ok
+  end
+
+  test "With explicit name" do
     Req.Test.stub(ExSnappy, fn conn ->
       Req.Test.text(conn, "OK")
     end)
 
     assert ExSnappy.snap("test-name", "<html></html>") == :ok
-  end
-
-  test "Strings script tags" do
-    assert ExSnappy.strip_script_tags("""
-             <html>
-               <script></script>
-               <script src="/app.js"></script>
-               <script type="module" src="/app-modern.esm"></script>
-             </html> 
-           """) == "<html></html>"
-  end
-
-  test "Wraps with html if missing <head>" do
-    Application.put_env(:ex_snappy, :wrapper_fn, fn inner_content ->
-      """
-      <!DOCTYPE html>
-      <html lang="en" class="[scrollbar-gutter:stable]">
-      <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link phx-track-static rel="stylesheet" href="/assets/app.css" />
-      </head>
-      <body class="bg-white antialiased dark:bg-gray-900 dark:text-gray-100">
-      #{inner_content}
-      </body>
-      </html>
-      """
-    end)
-
-    assert ExSnappy.maybe_wrap_html("<div></div>") ==
-             """
-             <!DOCTYPE html>
-             <html lang="en" class="[scrollbar-gutter:stable]">
-             <head>
-             <meta charset="utf-8" />
-             <meta name="viewport" content="width=device-width, initial-scale=1" />
-             <link phx-track-static rel="stylesheet" href="/assets/app.css" />
-             </head>
-             <body class="bg-white antialiased dark:bg-gray-900 dark:text-gray-100">
-             <div></div>
-             </body>
-             </html>
-             """
   end
 
   test "Passing along options" do
