6ce6e3b42dc238805e71d0e8694f4129c58575fe
Martin Feckie <martin@mfeckie.dev>
2025-04-17 13:29:09 +0800

    Get ready for publishing

--- mix.exs
+++ mix.exs
@@ -1,16 +1,20 @@
 defmodule ExSnappy.MixProject do
   use Mix.Project
+  @source_url "https://github.com/get-snappy/ex_snappy"
 
   def project do
     [
       app: :ex_snappy,
+      description:
+        "Phoenix and LiveView visual regression testing tool for the GetSnappy platform ",
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "ExSnappy",
-      source_url: "https://github.com/get-snappy/ex_snappy",
-      docs: &docs/0
+      source_url: @source_url,
+      docs: &docs/0,
+      package: package()
     ]
   end
 
@@ -25,6 +29,16 @@ defmodule ExSnappy.MixProject do
   defp docs do
     [
       extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Martin Feckie"],
+      links: %{
+        "GitHub" => @source_url
+      }
     ]
   end
 
